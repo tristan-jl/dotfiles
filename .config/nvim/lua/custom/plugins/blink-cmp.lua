ee2778f0adc5c43cb1b90a9b64a6441b912a923c
Tristan Laurens <tristanlaurens@hotmail.com>
2025-08-08 13:36:33 +0100

    Add copilot

--- .config/nvim/lua/custom/plugins/blink-cmp.lua
+++ .config/nvim/lua/custom/plugins/blink-cmp.lua
@@ -1,27 +1,48 @@
 return {
-  "saghen/blink.cmp",
-  dependencies = { "rafamadriz/friendly-snippets" },
-  version = "1.*",
+  {
+    "saghen/blink.cmp",
+    dependencies = { "rafamadriz/friendly-snippets", "fang2hou/blink-copilot" },
+    version = "1.*",
 
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  opts = {
-    keymap = { preset = "default" },
-    appearance = {
-      nerd_font_variant = "normal",
-    },
-    completion = { documentation = { auto_show = false } },
-    signature = { enabled = true },
-    sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
-      per_filetype = {
-        sql = { "snippets", "dadbod", "buffer" },
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      keymap = { preset = "default" },
+      appearance = {
+        nerd_font_variant = "normal",
       },
-      providers = {
-        dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+      completion = { documentation = { auto_show = false }, ghost_text = { enabled = true } },
+      signature = { enabled = true },
+      sources = {
+        default = { "lsp", "copilot", "path", "snippets", "buffer" },
+        per_filetype = {
+          sql = { "snippets", "dadbod", "buffer" },
+        },
+        providers = {
+          dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+          copilot = {
+            name = "copilot",
+            module = "blink-copilot",
+            score_offset = 100,
+            async = true,
+          },
+        },
       },
+      fuzzy = { implementation = "prefer_rust_with_warning" },
     },
-    fuzzy = { implementation = "prefer_rust_with_warning" },
+    opts_extend = { "sources.default" },
   },
-  opts_extend = { "sources.default" },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
+    },
+  },
 }
